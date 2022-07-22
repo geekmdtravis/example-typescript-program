44cabb640a1ac024a418d5a20d073bcb2e8f57b1
geekmdtravis <travis@geekmd.io>
2022-07-22 11:36:17 -0700

    changed handling of checking if user data has an error

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,11 @@ export default () => {
     setLoading(true);
     getUserData(1)
       .then((res) => {
-        if (userResolveDataHasError(res)) return;
+        const rv = getUserDataHasError(res);
+        if (rv.error) {
+          console.error(rv.msg);
+          return;
+        }
         setUser(res.DATA[0]);
       })
       .then((err) => console.error(err))
@@ -31,19 +35,22 @@ export default () => {
   );
 };
 
-function userResolveDataHasError(res: CclReturnData<User>): boolean {
+function getUserDataHasError(res: CclReturnData<User>): {
+  error: boolean;
+  msg?: string;
+} {
   console.log(res.META);
   if (!res.DATA) {
-    window.alert("no valid data was found for the user");
-    return true;
+    return { error: true, msg: "no valid data was found for the user" };
   }
   if (res.DATA.length === 0) {
-    window.alert("there was no data for the user");
-    return true;
+    return { error: true, msg: "there was no data for the user" };
   }
   if (res.DATA.length > 1) {
-    console.warn("more than one user was found; was expecting only one");
-    return true;
+    return {
+      error: true,
+      msg: "more than one user was found; was expecting only one",
+    };
   }
-  return false;
+  return { error: false };
 }
